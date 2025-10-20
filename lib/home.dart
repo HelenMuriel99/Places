d0744f8bb1b8f3e6d09bcc3b95dc52df2bf0960c
HelenMuriel99 <helenevelynmurielledezma@gmail.com>
2025-10-20 11:40:06 -0400

    Places

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:places/description_place.dart';
+import 'package:places/gradient_back.dart';
+import 'package:places/review.dart';
+import 'package:places/review_list.dart';
 
 class MyHome extends StatelessWidget{
   @override
@@ -9,22 +12,34 @@ class MyHome extends StatelessWidget{
 
     final descriptionPlace = Container(
       margin: EdgeInsets.only(
-        top: 250,
+        top: 50,
         left: 30,
         right: 30
       ),
       child: DescriptionPlace("Uyuni", 4, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. "),
     );
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "My Places"
-        ),
+
+    final reviewList = Container(
+      margin: EdgeInsets.only(
+          top: 20,
+          left: 30,
+          right: 30
       ),
+      child: ReviewList()
+    );
+
+    final listView = ListView(
+      children: <Widget>[
+        descriptionPlace,
+        reviewList
+      ],
+    );
+
+
+    return Scaffold(
       body: Stack(
         children: <Widget>[
-          descriptionPlace
+          GradientBack()
         ],
       ),
     );
